53f0642469da22c475acd44dd3e6eb034c168b7c
TanjuSimsek <tanju.simsek@yandex.com>
2019-02-20 15:04:53 +0300

    New

---
+++ MyPortfolio.Admin/Controllers/PostController.cs
@@ -0,0 +1,112 @@
+﻿using MyPortfolio.Model;
+using MyPortfolio.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService postService;
+        private readonly ICategoryService categoryService;
+        public PostController(IPostService postService, ICategoryService categoryService) {
+            this.categoryService = categoryService;
+            this.postService = postService;
+
+        }
+
+        // GET: Post
+        public ActionResult Index()
+        {
+            var post = postService.GetAll();
+            return View(post);
+        }
+        public ActionResult Create() {
+            var post = new Post();
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name");
+            return View(post);
+
+        }
+        
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                postService.Insert(post);
+
+                return RedirectToAction("Index");
+
+
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name", post.CategoryId);
+            return View();
+
+
+        }
+        public ActionResult Edit(Guid id) {
+
+            var post = postService.Find(id);
+            if (post == null) {
+
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name", post.CategoryId);
+            return View(post);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = postService.Find(post.Id);
+                model.Title = post.Title;
+                model.Description = post.Description;
+                model.CategoryId = post.CategoryId;
+                postService.Update(model);
+                return RedirectToAction("Index");
+
+
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name", post.CategoryId);
+            return View();
+
+
+        }
+        public ActionResult Delete(Guid id)
+        {
+
+
+
+            postService.Delete(id);
+            return RedirectToAction("Index");
+
+
+
+
+
+
+        }
+        public ActionResult Get(Guid id)
+        {
+
+
+
+
+            return View(postService.Find(id));
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ MyPortfolio.Model/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Model
+{
+    public class BaseEntity
+    {  // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
